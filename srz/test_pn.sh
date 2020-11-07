8f68fbc9d21b2b260905d4be64a00d9e51a54eb6
Mathieu Zimmermann <ma.zimmermann@unistra.fr>
2020-11-07 19:55:26 +0100

    pn fixe

---
+++ srz/test_pn.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Usage: rien du tout
+
+touch ./scripts/data
+
+echo -e "proba\tk\tnbStations\tnbSlots\tnbSlotsUtiles\tnbMessagesEnAttente" > ./scripts/data
+
+
+
+echo "nombre de slots max :"
+read nb_slots_max
+
+
+echo "nombre de stations :"
+read nb_stations
+
+echo "p*n min :"
+read pn_min
+
+echo "p*n max :"
+read pn_max
+
+echo "pas :"
+read pas
+
+echo "k :"
+read k
+
+./main_t u $nb_slots_max $nb_stations $pn_min $pn_max $pas $k >> ./scripts/data
+
+
+
---
+++ srz/test_pn2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Usage: rien du tout
+
+touch ./scripts/data
+
+echo -e "proba\tk\tnbStations\tnbSlots\tnbSlotsUtiles\tnbMessagesEnAttente" > ./scripts/data
+
+
+
+echo "nombre de slots max :"
+read nb_slots_max
+
+
+echo "proba :"
+read proba
+
+echo "p*n min :"
+read pn_min
+
+echo "p*n max :"
+read pn_max
+
+echo "pas :"
+read pas
+
+echo "k :"
+read k
+
+./main_t2 u $nb_slots_max $proba $pn_min $pn_max $pas $k >> ./scripts/data
+
+
+
